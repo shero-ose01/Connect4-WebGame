f2e7abfbe941a11c0cf9314a074bbc34e805cd02
Shero Ose <s.ose22601@gmail.com>
2026-06-29 16:56:40 +0200

    changed ui and added layout/footer to every page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home'
 import { Game } from './features/game/game'
-
+import { Layout } from './layout/layout'
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'game/:id', component: Game }
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Home },
+      { path: 'game/:id', component: Game }
+    ]
+  }
 ];
